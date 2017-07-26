3a39dd8574df10462e5ea7f0c38e7814d014db47
Manuel bonilla <josemanuelbonilla@gmail.com>
2017-07-26 10:32:18 +0200

    function to compute skew matrix from vector

--- include/simple_chain_ik/solvers/chainiksolverpos_relaxed.hpp
+++ include/simple_chain_ik/solvers/chainiksolverpos_relaxed.hpp
@@ -25,6 +25,19 @@
 #include <kdl/chainiksolver.hpp>
 #include <kdl/chainfksolver.hpp>
 #include <kdl/solveri.hpp>
+#include <Eigen/Core>
+
+inline void skew_symmetric(KDL::Vector &v_, Eigen::Matrix<double,3,3> &skew_mat_)
+{
+    skew_mat_ = Eigen::Matrix<double,3,3>::Zero();
+    
+    skew_mat_(0,1) = -v_(2);
+    skew_mat_(0,2) =  v_(1);
+    skew_mat_(1,0) =  v_(2);
+    skew_mat_(1,2) = -v_(0);
+    skew_mat_(2,0) = -v_(1);
+    skew_mat_(2,1) =  v_(0);
+}
 
 namespace KDL
 {
